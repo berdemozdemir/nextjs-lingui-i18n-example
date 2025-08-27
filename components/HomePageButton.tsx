ee72ed71bb2382bb6fc615917f8d682dc1850705
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-08-27 03:32:11 +0300

    feat: create homepage button

---
+++ components/HomePageButton.tsx
@@ -0,0 +1,16 @@
+import { Trans } from '@lingui/react/macro';
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
+
+export const HomePageButton = () => (
+  <Link
+    href="/"
+    className="absolute -top-5 left-0 flex items-center gap-1 text-xs text-gray-500 hover:underline"
+  >
+    <ArrowLeft size={14} />
+
+    <Trans id="home.button" context="navigation" comment="Home page button">
+      Home Page
+    </Trans>
+  </Link>
+);
