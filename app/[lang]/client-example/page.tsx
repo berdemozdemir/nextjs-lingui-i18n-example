395f4155172a670fa7a774311e847693162fec2e
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-09-07 01:14:36 +0300

    refactor: add example usage of t function in both client and server page

--- app/[lang]/client-example/page.tsx
+++ app/[lang]/client-example/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { Container } from '@/components/Container';
-import { Trans } from '@lingui/react/macro';
+import { Trans, useLingui } from '@lingui/react/macro';
 
 export default function Page() {
+  const { t } = useLingui();
+
   return (
     <Container showHomeButton>
       <h1 className="text-xl font-semibold tracking-tight text-gray-800 sm:text-2xl">
@@ -16,7 +18,8 @@ export default function Page() {
 
       <div className="inline-flex items-center gap-2 rounded-full border border-black/5 bg-white/60 px-3 py-1 text-xs text-gray-600">
         <span className="size-1.5 rounded-full bg-emerald-400"></span>
-        <Trans>Client rendered</Trans>
+
+        <p>{t`Client rendered`}</p>
       </div>
     </Container>
   );
--- components/ServerExamplePage.tsx
+++ components/ServerExamplePage.tsx
@@ -1,26 +1,30 @@
-import { Trans } from '@lingui/react/macro';
+import { Trans, useLingui } from '@lingui/react/macro';
 import { Container } from './Container';
 
-export const ServerExamplePage = () => (
-  <Container showHomeButton>
-    <h1 className="text-xl font-semibold tracking-tight text-gray-800 sm:text-2xl">
-      <Trans>Server side translated title</Trans>
-    </h1>
+export const ServerExamplePage = () => {
+  const { t } = useLingui();
 
-    <p className="mb-4 text-sm text-gray-600">
-      <Trans
-        id="server.description"
-        context="heading"
-        comment="main heading of page" // context and comment for translators in crowdin
-      >
-        Lingui + Next.js RSC example
-      </Trans>
-    </p>
+  return (
+    <Container showHomeButton>
+      <h1 className="text-xl font-semibold tracking-tight text-gray-800 sm:text-2xl">
+        <Trans>Server side translated title</Trans>
+      </h1>
 
-    <div className="inline-flex items-center gap-2 rounded-full border border-black/5 bg-white/60 px-3 py-1 text-xs text-gray-600">
-      <span className="size-1.5 rounded-full bg-emerald-400"></span>
+      <p className="mb-4 text-sm text-gray-600">
+        <Trans
+          id="server.description"
+          context="heading"
+          comment="main heading of page" // context and comment for translators in crowdin
+        >
+          Lingui + Next.js RSC example
+        </Trans>
+      </p>
 
-      <Trans>Server rendered</Trans>
-    </div>
-  </Container>
-);
+      <div className="inline-flex items-center gap-2 rounded-full border border-black/5 bg-white/60 px-3 py-1 text-xs text-gray-600">
+        <span className="size-1.5 rounded-full bg-emerald-400"></span>
+
+        <p>{t`Server rendered`}</p>
+      </div>
+    </Container>
+  );
+};
