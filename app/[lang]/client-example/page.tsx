691dbdd9851ccdb1a6385bcf5004596b547c388d
Berke Erdem Özdemir <103141951+berdemozdemir@users.noreply.github.com>
2025-09-07 20:18:44 +0300

    chore: update translations from Crowdin (#9)
    * chore: update German translations from Crowdin
    
    * chore: update Turkish translations from Crowdin

--- app/[lang]/client-example/page.tsx
+++ app/[lang]/client-example/page.tsx
@@ -20,10 +20,6 @@ export default function Page() {
         <span className="size-1.5 rounded-full bg-emerald-400"></span>
 
         <p>{t`Client rendered`}</p>
-
-        <p>{t`this is an example translation test text`}</p>
-
-        <Trans>this is an example translation test text22</Trans>
       </div>
     </Container>
   );
